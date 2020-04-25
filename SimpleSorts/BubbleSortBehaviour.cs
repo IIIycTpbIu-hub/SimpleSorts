fa69b2cc7c1ea639a84d8ecaf3e0c6350f05b6ee
IIIycTpbIu-hub <and95rey@yandex.ru>
2020-04-25 21:00:48 +0400

    Bubble sort class added

---
+++ SimpleSorts/BubbleSortBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSorts
+{
+    public class BubbleSortBehaviour<T> : ISortBehaviour<T> where T : IComparable
+    {
+        public T[] Sort(T[] arrayToSort)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
