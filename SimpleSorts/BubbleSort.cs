5a3c408a1dc520331678cace62aa38b1a2cc1b63
IIIycTpbIu-hub <and95rey@yandex.ru>
2020-04-25 20:02:31 +0400

    InitialCommit

---
+++ SimpleSorts/BubbleSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSorts
+{
+    public class BubbleSort<T> : BaseSort<T> where T : IComparable
+    {
+        public BubbleSort(T[] arrayToSort) : base(arrayToSort)
+        {
+
+        }
+    }
+}
